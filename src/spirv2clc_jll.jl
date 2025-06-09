859b53b514bfcb896411702cd030836fe8ca0f84
jlbuild <juliabuildbot@gmail.com>
2025-06-09 19:55:33 +0000

    spirv2clc_jll build 0.1.0+0

---
+++ src/spirv2clc_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule spirv2clc_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("spirv2clc")
+JLLWrappers.@generate_main_file("spirv2clc", UUID("f0274c0c-8c8a-59f1-85b7-f7d60330c5fb"))
+end  # module spirv2clc_jll
