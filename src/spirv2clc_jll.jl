d7d126f77da33f7226497623529e8569f0b4163a
jlbuild <juliabuildbot@gmail.com>
2026-06-24 21:14:59 +0000

    spirv2clc_jll build 0.2.0+0

--- src/spirv2clc_jll.jl
+++ src/spirv2clc_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("spirv2clc")
-JLLWrappers.@generate_main_file("spirv2clc", UUID("f0274c0c-8c8a-59f1-85b7-f7d60330c5fb"))
+JLLWrappers.@generate_main_file("spirv2clc", Base.UUID("f0274c0c-8c8a-59f1-85b7-f7d60330c5fb"))
 end  # module spirv2clc_jll
